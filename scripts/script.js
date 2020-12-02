7c23664a290489ce89ffae083c3c3ad3727fc77f
Anastasia Shilina <a.shilina29@gmail.com>
2020-12-02 12:34:37 +0200

    fix/ after review

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+
+const buttonOpen = document.querySelector('.profile__open-button');
+const buttonClose = document.querySelector('.popup__close-btn');
+const popup = document.querySelector('.popup');
+
+let profileInfoName = document.querySelector('.profile__info-name');
+let profileInfoAbout = document.querySelector('.profile__info-about');
+
+let popupForm = document.querySelector('.popup__form');
+let inputName = document.querySelector('.input-name');
+let inputAbout = document.querySelector('.input-about');
+
+let popupBtnAdd = document.querySelector('.popup__btn-add');
+
+function handleButtonOpenClick() {
+    popup.classList.add('popup_opened');
+    inputName.value = profileInfoName.textContent;
+    inputAbout.value = profileInfoAbout.textContent;
+}
+
+function handleButtonCloseClick() {
+    popup.classList.remove('popup_opened');
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+    profileInfoName.textContent = inputName.value;
+    profileInfoAbout.textContent = inputAbout.value;
+}
+popupForm.addEventListener('submit', handleFormSubmit);
+
+
+
+
+buttonOpen.addEventListener('click', handleButtonOpenClick);
+buttonClose.addEventListener('click', handleButtonCloseClick);
+popupForm.addEventListener('submit', handleFormSubmit);
+popupBtnAdd.addEventListener('click', handleButtonCloseClick);
+
+
+
+    
